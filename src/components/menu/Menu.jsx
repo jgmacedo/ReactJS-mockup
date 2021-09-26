7ba3ea433bde69d4f110c390eee1620ea59fcd08
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-09-26 20:48:12 -0300

    primeiras horas de projeto adicionadas

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,25 @@
+import "./menu.scss"
+
+export default function Menu() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <a href="#intro">home</a>
+                </li>
+                <li>
+                    <a href="#portfolio">portifólio</a>
+                </li>
+                <li>
+                    <a href="#works">trabalhos</a>
+                </li>
+                <li>
+                    <a href="#testimonials">depoimentos de clientes</a>
+                </li>
+                <li>
+                    <a href="#contact">contato</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import './topbar.scss'
+import {Person, Mail} from "@material-ui/icons"
+
+export default function Topbar({menuOpen,setMenuOpen}) {
+    return (
+        <div className={"topbar " + (menuOpen && "active")} id='topbar'>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className='logo'>jgmacedo.</a>
+                    <div className="itemContainer">
+                        <Person className='icon' />
+                        <span>+55 61 99997 1557</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className='icon' />
+                        <span>jgabreumacedo@gmail.com</span>
+                    </div>
+                </div>
+                
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
