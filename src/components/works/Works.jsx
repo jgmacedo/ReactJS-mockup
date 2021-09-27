41202478fc4fa151c6b39976eef8d780404b991e
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-09-27 19:16:02 -0300

    começado o slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,6 +3,25 @@ import './works.scss';
 const Works = () => {
     return (
         <div className='works' id='works'>
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="" />
+                                </div>
+                                <h2>title</h2>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum assumenda est.</p>
+                                <span>proctett</span>
+                            </div>
+                        </div>
+                        <div className="right">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
