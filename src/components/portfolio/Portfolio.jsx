587f4229b48c95781ba4c6b1edf1a57d00185381
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-09-27 12:45:59 -0300

    parei no 1:33:51, falta criar o array de dados.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,74 @@
-import './portfolio.scss'
+import { useState, useEffect } from 'react';
+import PortfolioList from '../portfolioList/PortfolioList';
+import './portfolio.scss';
 
 const Portfolio = () => {
+    
+    const [selected, setSelected] = useState("featured")
+
+    const list = [
+        {
+            id: "aplicativosweb",
+            title: "Aplicativos Web"
+
+        },
+        {
+            id: "design",
+            title: "Design"
+
+        }, {
+            id: "videos",
+            title: "Vídeos"
+
+        }, {
+            id: "fotografia",
+            title: "Fotografia"
+
+        }, {
+            id: "socialmedia",
+            title: "Social Media"
+
+        }
+    ]
+
     return (
         <div className="portfolio" id="portfolio">
+            <h1>Portifólio</h1>
+            <ul>
+                {list.map(item=>(
+                    <PortfolioList title={item.title} active={selected === item.id} setSelected={setSelected} id={item.id}/>
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <div className="img">
+                        <img src="https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg" alt="" />
+                        <h3>Banking app</h3>
+                    </div>
+                </div>
             
+            
+                <div className="item">
+                    <div className="img">
+                        <img src="https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg" alt="" />
+                        <h3>Banking app</h3>
+                    </div>
+                </div>
+            
+                <div className="item">
+                    <div className="img">
+                        <img src="https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg" alt="" />
+                        <h3>Banking app</h3>
+                    </div>
+                </div>
+            
+                <div className="item">
+                    <div className="img">
+                        <img src="https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg" alt="" />
+                        <h3>Banking app</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
