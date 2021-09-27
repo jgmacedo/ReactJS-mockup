de9a4a913f691f3f5721c1cc29ca513dac3f4b2b
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-09-26 23:15:42 -0300

    adicionadas animacoes de titulo e descer ao portifólio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
 import './intro.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
-const intro = () => {
+export default function Intro(){
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: false,
+            backDelay: 1500,
+            backSpeed:60,
+            strings: ['Webdesigner', 'Fotógrafo','Designer','Editor de vídeo', 'muito mais'],
+        });
+    },[])
+
     return (
         <div className='intro' id='intro'>
             <div className="left">
@@ -9,10 +23,16 @@ const intro = () => {
                 </div>
             </div>
             <div className="right">
+                <div className="wrapper">
+                    <h2>Olá, eu sou</h2>
+                    <h1>João Gabriel Abreu</h1>
+                    <h3>e sou <span ref={textRef}></span></h3>
 
+                </div>
+                <a href="#portfolio"><img src="assets/down.png" alt="" /></a>
             </div>
         </div>
     );
 }
+ 
 
-export default intro;
