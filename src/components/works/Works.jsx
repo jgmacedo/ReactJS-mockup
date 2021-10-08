2bf5f1a3c58eabe15bdb15bb31eb7690771f5ba6
Joao Gabriel Abreu Macedo <jgabreumacedo@gmail.com>
2021-10-08 09:37:13 -0300

    corrigido slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,78 +1,88 @@
-import { useState } from 'react';
-import './works.scss';
+import { useState } from "react";
+import "./works.scss";
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    const [currentSlide, setCurrentSlide] = useState(0)
+  const data = [
+    {
+      id: "0",
+      icon: "./assets/writing.png",
+      title: "bengas",
+      desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
+      img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
+      type: "picici",
+    },
+    {
+      id: "1",
+      icon: "./assets/globe.png",
+      title: "Global",
+      desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
+      img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
+      type: "picas",
+    },
+    {
+      id: "2",
+      icon: "./assets/twitter.png",
+      title: "Social Media",
+      desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
+      img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
+      type: "post",
+    },
+    {
+      id: "3",
+      icon: "./assets/linkedin.png",
+      title: "LinkedIn",
+      desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
+      img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
+      type: "blimblom",
+    },
+  ];
 
-    const data =[
-        {
-            id: "0",
-            icon: "./assets/writing.png",
-            title: "bengas",
-            desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
-            img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
-            type: "picici"
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
 
-        },
-        {
-            id: "1",
-            icon: "./assets/globe.png",
-            title: "Global",
-            desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
-            img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
-            type: "picas"
-        },
-        {
-            id: "2",
-            icon: "./assets/twitter.png",
-            title: "Social Media",
-            desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
-            img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
-            type: "post"
-        },
-        {
-            id: "3",
-            icon: "./assets/linkedin.png",
-            title: "LinkedIn",
-            desc: "lorem ipsum dolor sit amet, consectetur adipiscing elit edamame",
-            img: "https://i.ytimg.com/vi/HghA2Kh-TBc/maxresdefault.jpg",
-            type: "blimblom"
-        }
-    ]
-
-    const handleClick = (way)=>{
-        way === "left"
-            ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 3)  
-            : setCurrentSlide(currentSlide < data.lenght - 1 ? currentSlide + 1 : 0);
-    };
-
-    return (
-        <div className='works' id='works'>
-            <div className="slider" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
-                {data.map(d=>(
-                    <div className="container">
-                    <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-                                <div className="imgContainer">
-                                    <img src="{d.icon}" alt="" />
-                                </div>
-                                <h2>{d.title}</h2>
-                                <p>{d.desc}</p>
-                                <span>{d.type}</span>
-                            </div>
-                        </div>
-                        <div className="right">
-                           <img src={d.img} alt="" /> 
-                        </div>
-                    </div>
-                </div>))}
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>{d.type}</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
+              </div>
             </div>
-            <img src="assets/down.png" className="arrow left" alt="" onClick={()=>handleClick()} />
-            <img src="assets/down.png" className="arrow right" alt="" onClick={()=>handleClick("left")} />
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/down.png"
+        className="arrow left"
+        alt=""
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/down.png"
+        className="arrow right"
+        alt=""
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
-
-
