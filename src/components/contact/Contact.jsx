ac8f672caea00e3beb6d18013b14c23247b7597f
João Gabriel Abreu <jgabreumacedo@gmail.com>
2021-10-15 10:46:07 -0300

    finalizado responsividade mobile

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import "./contact.scss";
 
 const Contact = () => {
+  const [message, setMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage(true);
+  };
+
   return (
     <div className="contact" id="contact">
       <div className="left">
-        <img src="assets/hello.png" alt="" />
+        <img src="assets/hands.svg" alt="" />
       </div>
 
       <div className="right">
         <h2>Contato.</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Email" />
           <textarea placeholder="Mensagem"></textarea>
           <button type="submit">Enviar</button>
+          {message && <span>Obrigado, responderei assim que possível :]</span>}
         </form>
       </div>
     </div>
